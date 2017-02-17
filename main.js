7fcb7790a6969655c777742559c87ddf24e0ef83
Sovianum <aklyukvin@yandex.com>
2017-02-17 17:12:50 +0300

    Finally working

--- main.js
+++ main.js
@@ -9,13 +9,18 @@ let lib = require("./server_lib");
 
 let urlDict = {
     "/index/": new lib.BindedFileFunc("./static/html/index.html"),  // particular file is binded to particular URL
-    "/main/": new lib.BindedFileFunc("./static/html/main_page.html"),
-    "/cube/": new lib.BindedFileFunc("./static/html/cube_page.html"),
+    "/about/": new lib.BindedFileFunc("./static/html/about.html"),
+    "/leaders/": new lib.BindedFileFunc("./static/html/leaders.html"),
+    "/login/": new lib.BindedFileFunc("./static/html/login.html"),
+    "/signup/": new lib.BindedFileFunc("./static/html/signup.html"),
+    "/": new lib.BindedFileFunc("./static/html/index.html"),
+
 };
 urlDict[lib.getRegexStr("/static_js/")] = new lib.FolderFileFunc("./static/js/", "/static_js/"); // particular folder is binded to regexp
 urlDict[lib.getPrefixStr("/static/")] = new lib.FolderFileFunc("./static/", "/static/");    // particular folder is binded to prefix
 
 let server = lib.getStaticServer(urlDict);
 
-server.listen(8083);
+let port = process.env.PORT || 3000;
+server.listen(port);
 
