46e555edbfc698b204108d2f4780d6299e084860
Антон Телышев <anton.telyshev@yandex.ru>
2017-02-20 13:13:35 +0300

    Testing example

---
+++ tests/test.js
@@ -0,0 +1,38 @@
+'use strict';
+
+
+const PORT = 3000;
+const SERVER_URL = `http://localhost:${PORT}`
+const HTTP_CODE_OK = 200;
+
+
+let assert = require('assert');
+let http = require('http');
+let lib = require('../server_lib.js');
+
+
+function _getHtmlFilePath(filename) {
+	return `${__dirname}/../static/html/${filename}`
+}
+
+
+const urlDict = {'/': new lib.BindedFileFunc(_getHtmlFilePath('index.html'))};
+
+
+let staticServer = lib.getStaticServer(urlDict);
+
+
+describe('Static server testing', () => {
+	before(() => staticServer.listen(PORT));
+
+	describe('/', () => {
+		it('should return 200', (done) => {
+		    http.get(SERVER_URL, (result) => {
+				assert.equal(HTTP_CODE_OK, result.statusCode);
+				done();
+			});
+		});
+	});
+
+	after(() => staticServer.close());
+});
