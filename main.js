871e473afbe4306bafa34d724da383bb160309f3
Meganster <ppro100takk@mail.ru>
2017-02-23 22:42:01 +0300

    Update structure and interface

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-/**
- * Created by artem on 2/12/17.
- */
-
 "use strict";
+let lib = require("./core/server_lib");
 
-let lib = require("./server_lib");
+const DEFAULT_PORT = 3000;
 
+let router = new lib.Router();
 
-let urlDict = {
-    "/index/": new lib.BindedFileFunc("./static/html/index.html"),  // particular file is binded to particular URL
-    "/main/": new lib.BindedFileFunc("./static/html/main_page.html"),
-    "/cube/": new lib.BindedFileFunc("./static/html/cube_page.html"),
-};
-urlDict[lib.getRegexStr("/static_js/")] = new lib.FolderFileFunc("./static/js/", "/static_js/"); // particular folder is binded to regexp
-urlDict[lib.getPrefixStr("/static/")] = new lib.FolderFileFunc("./static/", "/static/");    // particular folder is binded to prefix
+const PLAIN_URLS = ['index', 'about', 'leaders', 'login', 'singin', 'signup'];
+PLAIN_URLS.forEach((url) => {
+    router.addRegexURL(`^/${url}/?$`, new lib.BindedFile(`./html/${url}.html`));
+});
 
-let server = lib.getStaticServer(urlDict);
+router.addPlainURL("/", new lib.BindedFile("./html/index.html"));
+router.addRegexURL("^/game/?$", new lib.BindedFile("./html/game_logic_test.html"));
 
-server.listen(8083);
+router.addRegexURL("^/lib/.*\.js$", new lib.BindedFolder("./static/_lib/js/", "/lib/"));
+router.addRegexURL("^/lib/.*\.css$", new lib.BindedFolder("./static/_lib/css/", "/lib/"));
+
+router.addRegexURL(".*\.js$", new lib.BindedFolder("./static/js"));
+router.addRegexURL(".*\.css$", new lib.BindedFolder("./static/css"));
+
+router.addRegexURL("^/media/", new lib.BindedFolder("./static/media/", "/media/"));
+router.addRegexURL("^/core/.*\.js$", new lib.BindedFolder("./core/", "/core/"));
+router.addRegexURL("^/core/lib/.*\.js$", new lib.BindedFolder("./core/_lib/", "/core/lib/"));
+
+let server = lib.getStaticServer(router);
+
+let port = process.env.PORT || DEFAULT_PORT;
+server.listen(port);
 
