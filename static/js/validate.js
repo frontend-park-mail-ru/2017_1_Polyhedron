cc01893c61996221159508fef90818142cffc91b
Meganster <ppro100takk@mail.ru>
2017-03-07 20:55:21 +0300

    Update alignment in validators code

--- static/js/validate.js
+++ static/js/validate.js
@@ -9,141 +9,141 @@ const MAX_LENGTH = 14;
 
 
 class Data {
-  resetError(key){
-	switch(key){
-	case 'login':
-		errorLogin.innerHTML='';
-		this.error.errorLogin = true;
-		break;
-	case 'password':
-		errorPassword.innerHTML='';
-		this.error.errorPass = true;		
-		break;
-	case 'password2':
-		errorPassword2.innerHTML='';
-		this.error.errorPass2 = true;	
-		break;
-	case 'email':
-		errorEmail.innerHTML='';
-		this.error.errorEmail = true;	
-		break;
-      }
-  }
-
-  checkSubmitButton(){
-	let result = false;
-	for (let key in this.error) {
-		result +=  this.error[key];
+	resetError(key){
+		switch(key){
+			case 'login':
+			errorLogin.innerHTML='';
+			this.error.errorLogin = true;
+			break;
+			case 'password':
+			errorPassword.innerHTML='';
+			this.error.errorPass = true;		
+			break;
+			case 'password2':
+			errorPassword2.innerHTML='';
+			this.error.errorPass2 = true;	
+			break;
+			case 'email':
+			errorEmail.innerHTML='';
+			this.error.errorEmail = true;	
+			break;
+		}
 	}
 
-	if(!result){
-		submitForm.disabled = false;
-	} else {
-		submitForm.disabled = true;
+	checkSubmitButton(){
+		let result = false;
+		for (let key in this.error) {
+			result +=  this.error[key];
+		}
+
+		if(!result){
+			submitForm.disabled = false;
+		} else {
+			submitForm.disabled = true;
+		}
 	}
-  }
 
-  validateLogin(){    
-   let login = document.getElementById('username').value;
-   this.login = login; 
-   this.resetError('login');
+	validateLogin(){    
+		let login = document.getElementById('username').value;
+		this.login = login; 
+		this.resetError('login');
 
-   if (login.length == 0){
- 	errorLogin.innerHTML='Данное поле обязательно для заполнения';
-   } else {
-	   if (login.length > MAX_LENGTH){
-	 	errorLogin.innerHTML = 'Поле должно содержать меньше ' + MAX_LENGTH + ' символов';
-	   } else {
-		   if ( !loginAndPassword_pattern.test(login) ){
-		 	errorLogin.innerHTML='Данное поле должно содержать только символы A-z, 0-9 и _';
-		   } else {
-			this.error.errorLogin = false;
-		   }
-	   }
-    }
-   this.checkSubmitButton();						
-  }
+		if (login.length == 0){
+			errorLogin.innerHTML='Данное поле обязательно для заполнения';
+		} else {
+			if (login.length > MAX_LENGTH){
+				errorLogin.innerHTML = 'Поле должно содержать меньше ' + MAX_LENGTH + ' символов';
+			} else {
+				if ( !loginAndPassword_pattern.test(login) ){
+					errorLogin.innerHTML='Данное поле должно содержать только символы A-z, 0-9 и _';
+				} else {
+					this.error.errorLogin = false;
+				}
+			}
+		}
+		this.checkSubmitButton();						
+	}
 
-  validatePassword(){    
-   let password = document.getElementById('password').value;
-   this.password = password;
-   this.resetError('password');
+	validatePassword(){    
+		let password = document.getElementById('password').value;
+		this.password = password;
+		this.resetError('password');
 
-   if (password.length == 0){
- 	errorPassword.innerHTML='Данное поле обязательно для заполнения';
-   } else {
-	   if (password.length > MAX_LENGTH){
-	 	errorPassword.innerHTML='Поле должно содержать меньше ' + MAX_LENGTH + ' символов';
-	   } else {
-		   if (!loginAndPassword_pattern.test(password)) {
-		 	errorPassword.innerHTML='Данное поле должно содержать только символы A-z, 0-9 и _';
-		   } else {
-			this.error.errorPass = false;
-		   }
-	  }
-    }
-   this.checkSubmitButton();
-  }
+		if (password.length == 0){
+			errorPassword.innerHTML='Данное поле обязательно для заполнения';
+		} else {
+			if (password.length > MAX_LENGTH){
+				errorPassword.innerHTML='Поле должно содержать меньше ' + MAX_LENGTH + ' символов';
+			} else {
+				if (!loginAndPassword_pattern.test(password)) {
+					errorPassword.innerHTML='Данное поле должно содержать только символы A-z, 0-9 и _';
+				} else {
+					this.error.errorPass = false;
+				}
+			}
+		}
+		this.checkSubmitButton();
+	}
 
-  validatePassword2(){    
-   let password2 = document.getElementById('password2').value;
-   this.resetError('password2');
+	validatePassword2(){    
+		let password2 = document.getElementById('password2').value;
+		this.resetError('password2');
 
-   if (this.password != password2) {
-	errorPassword2.innerHTML='Пароли должны совпадать';
-   } else {
-	this.error.errorPass2 = false;
-   }
-   this.checkSubmitButton();
-  }
+		if (this.password != password2) {
+			errorPassword2.innerHTML='Пароли должны совпадать';
+		} else {
+			this.error.errorPass2 = false;
+		}
+		this.checkSubmitButton();
+	}
 
-  validateEmail(){    
-   let email = document.getElementById('email').value;
-   this.email = email;
-   this.resetError('email');
+	validateEmail(){    
+		let email = document.getElementById('email').value;
+		this.email = email;
+		this.resetError('email');
 
-   if (email.length == 0){
- 	errorEmail.innerHTML='Данное поле обязательно для заполнения';
-   } else {
-	   if (email.length > MAX_LENGTH){
-	 	errorEmail.innerHTML='Поле должно содержать меньше ' + MAX_LENGTH + ' символов';
-	   } else {
-		   if ( !email_pattern.test(email) ){
-		 	errorEmail.innerHTML='Данное поле не является валидным email';
-		   } else {
-			this.error.errorEmail = false;
-		   }
-	   }
-    }
-    this.checkSubmitButton();			
-  }
+		if (email.length == 0){
+			errorEmail.innerHTML='Данное поле обязательно для заполнения';
+		} else {
+			if (email.length > MAX_LENGTH){
+				errorEmail.innerHTML='Поле должно содержать меньше ' + MAX_LENGTH + ' символов';
+			} else {
+				if ( !email_pattern.test(email) ){
+					errorEmail.innerHTML='Данное поле не является валидным email';
+				} else {
+					this.error.errorEmail = false;
+				}
+			}
+		}
+		this.checkSubmitButton();			
+	}
 
-  constructor(form) {
-    let elems = form.elements;
-    this.login = "";
-    this.email = "";
-    this.password = "";
-    this.error = {};
+	constructor(form) {
+		let elems = form.elements;
+		this.login = "";
+		this.email = "";
+		this.password = "";
+		this.error = {};
 
-    //submitForm.addEventListener("click", () => this.sendData());
+	    //submitForm.addEventListener("click", () => this.sendData());
 
-    if(elems.login != null){
-    	elems.login.addEventListener("change", () => this.validateLogin());
-	this.error.errorLogin = true;
-    }
-    if(elems.email != null){
-    	elems.email.addEventListener("change", () => this.validateEmail());
-	this.error.errorEmail = true;
-    }
-    if(elems.password != null){
-    	elems.password.addEventListener("change", () => this.validatePassword());
-	this.error.errorPass = true;
-    }
-    if(elems.password2 != null){
-    	elems.password2.addEventListener("change", () => this.validatePassword2());
-	this.error.errorPass2 = true;
-    }
-  }
+	    if(elems.login != null){
+	    	elems.login.addEventListener("change", () => this.validateLogin());
+	    	this.error.errorLogin = true;
+	    }
+	    if(elems.email != null){
+	    	elems.email.addEventListener("change", () => this.validateEmail());
+	    	this.error.errorEmail = true;
+	    }
+	    if(elems.password != null){
+	    	elems.password.addEventListener("change", () => this.validatePassword());
+	    	this.error.errorPass = true;
+	    }
+	    if(elems.password2 != null){
+	    	elems.password2.addEventListener("change", () => this.validatePassword2());
+	    	this.error.errorPass2 = true;
+	    }
+	}
 };
 
 (function(){
