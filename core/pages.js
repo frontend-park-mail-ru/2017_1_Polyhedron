51c8a2484b577cd2f9189f6a05cc849653e42930
Irene Lukyanova <irene@aithelle.com>
2017-03-08 23:15:42 +0300

    2.1

--- core/pages.js
+++ core/pages.js
@@ -1,13 +1,16 @@
 'use strict';
-
 (function () {
-    const Page = require('/static/js/page').Page;
-    class Index extends Page {
+
+    class Page {
+        constructor (options) {
+            this.options = options;
+        };
+
         render () {
             this.options.subheader.innerHTML = "Многопользовательский пинг-понг";
             this.options.content.innerHTML = window.render_index(this.options);
         };
     }
 
-    module.exports.Index = Index;
+    module.exports.Page = Page;
 })();
