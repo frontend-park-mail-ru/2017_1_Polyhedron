b355a860a08d676eefe8e863d1738c855720020d
Irene Lukyanova <irene@aithelle.com>
2017-03-10 00:47:56 +0300

    game placement corrected

--- static/js/pages/game.js
+++ static/js/pages/game.js
@@ -2,12 +2,12 @@
 
 const BasePage = require('./base');
 const createGame = require('../../../core/game_start');
-createGame('game');
 
 class Game extends BasePage {
     render () {
         this._heading.innerHTML = "Игра";
         this._content.innerHTML = this._template(this._options);
+        createGame('game');
     }
 }
 
