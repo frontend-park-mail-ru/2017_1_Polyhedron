225e34861ff0991f65b59ef9f72f8428cad946f9
Meganster <ppro100takk@mail.ru>
2017-03-09 21:03:46 +0300

    Update validator

--- static/js/validate.js
+++ static/js/validate.js
@@ -1,3 +1,15 @@
+const loginFields = {
+    email: "email",
+    password: "password",
+    submitFormButton: "submitSignInButton"
+}
+const registrationFields = {
+    email: "email",
+    login: "login",
+    password: "password",
+    password2: "password2",
+    submitFormButton: "submitSignUpButton"
+}
 const patterns = {
 	loginPattern: /^[a-z][a-z0-9]*?([_][a-z0-9]+){0,2}$/i,
 	passwordPattern: /[_a-zA-Z0-9]$/i,
@@ -10,47 +22,48 @@ const error = {
 	password2: document.getElementById('errorPassword2'),
 	email: document.getElementById('errorEmail')
 }
-const submitFormButton = document.getElementById('submitFormButton');
+
 const MAX_LENGTH = 14;
+
+const errorLabel = {
+    nullLength: 'Данное поле обязательно для заполнения',
+    overflowLength: 'Поле должно содержать меньше ' + MAX_LENGTH + ' символов',
+    symbols: 'Поле должно содержать только символы A-z, 0-9 и _',
+    email: 'Поле не является валидным e-mail',
+    password: 'Пароли не совпадают'
+}
 
 
 class Form{
-	constructor(form) {
-        this.fields = {
-        	'login': {
-        		name: "login",
-        		value: false,
-        		error: true
-        	}, 
-         	'email': {
-         		name: "email",
-        		value: false,
-        		error: true
-        	},
-         	'password': {
-         		name: "password",
-        		value: false,
-        		error: true
-        	},
-         	'password2': {
-         		name: "password2",
-        		value: false,
-        		error: true
-        	}
-        };
-
-        if(submitFormButton != null){
-			submitFormButton.addEventListener("click", () => this.checkToSend());
+	constructor(form, fields) {
+	    this.fields = {};
+		for (let key in fields) {
+		    if(key != 'submitFormButton') {
+                this.fields[key] = {};
+                this.fields[key].value = false;
+                this.fields[key].error = true;
+            }
 		}
+
+        let submitFormButton = document.getElementById(fields["submitFormButton"]);
+        if(submitFormButton != null){
+            submitFormButton.addEventListener("click", () => this.validateBeforeSubmit());
+        }
 
         let elems = form.elements;
         for (let key in  this.fields) {
-        	let nameField = this.fields[key].name;
-        	if (nameField in elems){
-				elems[nameField].addEventListener("change", () => this.validate(nameField));
-				submitFormButton.addEventListener("click", () => this.validate(nameField));
+        	if (key in elems){
+				elems[key].addEventListener("change", () => this.validate(key));
 			}
 		}
+    }
+
+    validateBeforeSubmit(){
+        for (let key in  this.fields) {
+                this.validate(key);
+        }
+
+        this.checkToSend();
     }
 
     resetError(key){
@@ -64,63 +77,71 @@ class Form{
 		this.resetError(nameField);
 
 		if (valueField.length == 0){
-			error[nameField].innerHTML='Данное поле обязательно для заполнения';
+			error[nameField].innerHTML = errorLabel.nullLength;
 		} else if (valueField.length > MAX_LENGTH){
-					error[nameField].innerHTML = 'Поле должно содержать меньше ' + MAX_LENGTH + ' символов';
-				} else if ( !patterns[nameField + 'Pattern'].test(valueField) ){
-							if (nameField == 'email') {
-								error[nameField].innerHTML='Поле не является валидным e-mail';
-							} else {
-								error[nameField].innerHTML='Поле должно содержать только символы A-z, 0-9 и _';
-							}
-						} else {
-							this.fields[nameField].error = false;
-						}
+					error[nameField].innerHTML = errorLabel.overflowLength;
+				} else if ( nameField == 'password2' && this.fields['password'].value != valueField) {
+                                error['password2'].innerHTML = errorLabel.password;
+                        } else if ( !patterns[nameField + 'Pattern'].test(valueField) ){
+                                    if (nameField === 'email') {
+                                        error[nameField].innerHTML = errorLabel.email;
+                                    } else {
+                                        error[nameField].innerHTML = errorLabel.symbols;
+                                    }
+                                } else {
+                                    this.fields[nameField].error = false;
+                                }
     }
 
 	checkToSend(){
 		//need to override
 		console.log("Need to override 'checkToSend()' method");
+		throw "Need to override 'checkToSend()' method";
 	}
 
 	sendData(){
 		//need to override
 		console.log("Need to override 'sendData()' method");
+        throw "Need to override 'checkToSend()' method";
 	}
 };
 
 class SignUpForm extends Form{
 	checkToSend(){
-		let result = false;
+		let errors = false;
 		for (let key in this.fields) {
-			result +=  this.fields[key].error;
+                errors =  errors || this.fields[key].error;
 		}
 
-		if (result == false){
+		if (!errors){
 			this.sendData();
 		}
 	}
 
 	sendData(){
-		let session = new ClientServerAPI('/api/user/registration');
+		let session = new ClientServerAPI();
 		session.register(this.fields['email'].value, 
 						 this.fields['login'].value, 
-						 this.fields['password'].value);
+						 this.fields['password'].value).
+		then(data => {}).catch(err => {}); //then - succses, err - fail
+        // TODO write promise
 	}
 };
 
 class SignInForm extends Form{
 	checkToSend(){
-		let result = this.fields['email'].error || this.fields['password'].error;
+		let errors = this.fields['email'].error || this.fields['password'].error;
 
-		if (result == false){
+		if (!errors){
 			this.sendData();
 		}
 	}
 
 	sendData(){
-		let session = new ClientServerAPI('/api/user/login');
-		session.login(this.fields['login'].value, this.fields['password'].value);
+		let session = new ClientServerAPI();
+		session.login(this.fields['email'].value, this.fields['password'].value)
+		.then(data => {}).catch(err => {}); //then - succses, err - fail
+        // TODO write promise
 	}
 };
 
@@ -133,20 +154,13 @@ class SignInForm extends Form{
 		console.log("Error, form not found");
 	} else {
 		if(signInForm != null){
-			// Переписать блок при помощи модулей и require.js
-			let dataSignInForm = new SignInForm(signInForm);
+			// TODO Переписать блок при помощи модулей и require.js
+			let dataSignInForm = new SignInForm(signInForm, loginFields);
 			document.body.userData = dataSignInForm; //Добавим в DOM
 		} else {
-			// Переписать блок при помощи модулей и require.js
-			let dataSignUpForm = new SignUpForm(signUpForm);
+			// TODO Переписать блок при помощи модулей и require.js
+			let dataSignUpForm = new SignUpForm(signUpForm, registrationFields);
 			document.body.userData = dataSignUpForm; //Добавим в DOM
 		}
 	}
-	/*let signInForm = document.getElementById("signInForm");
-	if(signInForm == null){
-		console.log("Error, form not found");
-	} else {
-			let dataSignInForm = new Form(signInForm);
-			document.body.userData = dataSignInForm; //Добавим в DOM
-	}*/
 })();
