4998263d84a549fa1e4f073a66bc8bc45c8afdae
Sovianum <aklyukvin@yandex.com>
2017-04-05 03:25:14 +0300

    Small restructuring

--- compilers/pug_compiler.js
+++ compilers/pug_compiler.js
@@ -1,7 +1,7 @@
 
 const TEMPLATE_DIR = './templates';
 const RESULT_DIR = './static/js/templates';
-const compileAll = require('./core/server_side/service/template_compilation');
+const compileAll = require('./../core/server_side/service/template_compilation');
 
 (function () {
     compileAll(TEMPLATE_DIR, RESULT_DIR);
