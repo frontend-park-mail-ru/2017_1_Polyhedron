4f77714b030d17369c0fba74ee5e6e389a2e5812
Sovianum <aklyukvin@yandex.com>
2017-05-02 13:33:18 +0300

    Ignored unnecessary files

--- core/common/id_generator.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-function getIdGenerator() {
-    let id = 0;
-    return () => ++id;
-}
-exports.getIdGenerator = getIdGenerator;
-//# sourceMappingURL=id_generator.js.map
