a97e44c370fc2e66168f67ec84d8557197c0989d
Sovianum <aklyukvin@yandex.com>
2017-05-21 21:17:58 +0300

    Testing docker

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ $TRAVIS_BRANCH == 'master' ]
+then
     docker login --username=_ --password=$(echo $API_KEY) registry.heroku.com
     docker build -t registry.heroku.com/polyhedron/web .
     docker push registry.heroku.com/polyhedron/web
