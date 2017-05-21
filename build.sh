e6a5a6d3fc06d184c401dafe9d46a86fc6d57e9b
Sovianum <aklyukvin@yandex.com>
2017-05-21 21:41:37 +0300

    Testing docker

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ then
     docker push registry.heroku.com/polyhedron/web
 else
     docker login --username=_ --password=$(echo $API_KEY) registry.heroku.com
-    docker build -t registry.heroku.com/polyhedron/web .
-    docker push registry.heroku.com/polyhedron/web
+    docker build -t registry.heroku.com/polyhedron-team/web .
+    docker push registry.heroku.com/polyhedron-team/web
 fi
 
