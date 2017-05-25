3c7f517e1b8acb00e5ea4692f2fd50c20f423ba4
Sovianum <aklyukvin@yandex.com>
2017-05-26 00:23:56 +0300

    Uniginored worker script

---
+++ core/client_side/site_service/offline_mode/worker_script.js
@@ -0,0 +1,31 @@
+'use strict';
+const CACHE_NAME = 'cache_v_1';
+const INFO_URL = '/cached_urls';
+self.addEventListener('install', event => {
+    event.waitUntil(fetch(INFO_URL)
+        .then(response => response.json())
+        .then(cacheURLs => {
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(cacheURLs));
+    }));
+});
+self.addEventListener('fetch', event => {
+    // TODO maybe need to enable cors (check after deploy to heroku)
+    const lastModifiedPromise = fetch(event.request.url, {
+        method: 'HEAD'
+    })
+        .then(response => response.headers.get('Last-Modified'))
+        .catch(err => null);
+    const cachedResponsePromise = caches.match(event.request);
+    event.respondWith(Promise
+        .all([lastModifiedPromise, cachedResponsePromise])
+        .then(([lastModified, cachedResponse]) => {
+        if (cachedResponse) {
+            if (!lastModified || new Date(lastModified).getTime() < Date.now()) {
+                return cachedResponse;
+            }
+        }
+        return fetch(event.request);
+    }));
+});
+//# sourceMappingURL=worker_script.js.map
